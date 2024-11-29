0268d9a1c4315e180e96ff8bb7d077b6cc39ebd2
Simon BISCH <simon.bisch@free.fr>
2024-11-29 17:13:04 +0100

    Add CI.sh

---
+++ CI.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+source ~/.bashrc
+
+app_name=$1
+app_version=$2
+
+export ARGOCD_SERVER='https://localhost:8080'
+export ARGOCD_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJkZXBsb3ktYWdlbnQ6YXBpS2V5IiwibmJmIjoxNzMyODc1NzEzLCJpYXQiOjE3MzI4NzU3MTMsImp0aSI6IjA2MDEwZGY1LTg3NDUtNGMzNi1iMDg2LTM4OWJiODE5NDhjZCJ9.8W_KF3HnhpfGG3hrtl4sqvhi9fgJvUsWm8utdUwWlI0"
+
+
+argocd app get $app_name --server $ARGOCD_SERVER --auth-token "$ARGOCD_TOKEN"
+if [ $? -ne 0 ]
+then
+    echo "l'application $app_name n'existe pas"
+    exit 1
+fi
+
+argocd app set $app_name --parameter image.tag=$app_version --server "$ARGOCD_SERVER" --auth-token "$ARGOCD_TOKEN"
+if [ $? -ne 0 ]
+then
+    echo "Echec du changement de version de $app_name. Version cible $app_version"
+    exit 1
+fi
+
+argocd app sync $app_name --server "$ARGOCD_SERVER" --auth-token "$ARGOCD_TOKEN"
+if [ $? -ne 0 ]
+then
+    echo "Echec de la synchronisation de $app_name. Version cible $app_version"
+    exit 1
+fi
+
+argocd app wait $app_name --server "$ARGOCD_SERVER" --auth-token "$ARGOCD_TOKEN"
